d524f7777c64ccf8e6a11e2f9357a56c05d75ffa
Emiliano Perez <emi.perez997@gmail.com>
2025-01-06 20:27:58 -0300

    [ADD] Clase 6

--- clase_4/github-example/src/models/user.model.js
+++ clase_4/github-example/src/models/user.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   age: { type: Number, required: true },
   password: { type: String },
   githubId: { type: String },
